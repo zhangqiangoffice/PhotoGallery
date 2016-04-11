abec8c85cc8354303476cccfe6b7596ceb410f86
ZhangQiang <zhangqiangoffice@163.com>
2016-04-11 15:31:09 +0800

    从百度获取网页

--- src/com/example/photogallery/PhotoGalleryFragment.java
+++ src/com/example/photogallery/PhotoGalleryFragment.java
@@ -1,19 +1,26 @@
 package com.example.photogallery;
 
+import java.io.IOException;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
+	private static final String TAG = "photoGalleryFragment";
+	
 	GridView mGridView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setRetainInstance(true);
+		new FetchItemsTask().execute();
 	}
 
 	@Override
@@ -26,5 +33,17 @@ public class PhotoGalleryFragment extends Fragment {
 		return v;
 	}
 	
+	private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				String result = new FlickrFetchr().getUrl("http://www.baidu.com");
+				Log.i(TAG, "Fetched contents of URL: " + result);
+			} catch (IOException ioe) {
+				Log.e(TAG, "Failed to fetch URL: ", ioe);
+			}
+			return null;
+		}
+	}
 	
 }
