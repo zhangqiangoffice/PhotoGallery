b447d21a7228c2296ccbf859b4aa06344e86fa19
ZhangQiang <zhangqiangoffice@163.com>
2016-04-11 13:37:53 +0800

    新建基础布局

--- src/com/example/photogallery/PhotoGalleryActivity.java
+++ src/com/example/photogallery/PhotoGalleryActivity.java
@@ -1,22 +1,11 @@
 package com.example.photogallery;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
 
-public class PhotoGalleryActivity extends Activity {
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_photo_gallery);
+	public Fragment createFragment() {
+		return new PhotoGalleryFragment();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.photo_gallery, menu);
-		return true;
-	}
-
 }
---
+++ src/com/example/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,30 @@
+package com.example.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class PhotoGalleryFragment extends Fragment {
+	GridView mGridView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+		
+		mGridView = (GridView) v.findViewById(R.id.gridView);
+		// TODO Auto-generated method stub
+		return v;
+	}
+	
+	
+}
