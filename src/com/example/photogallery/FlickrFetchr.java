21c6ccf20a8db684795d73bc1348eb36b9a00f35
ZhangQiang <zhangqiangoffice@163.com>
2016-04-11 16:03:26 +0800

    由于Flickr在国内被封，所以网络请求总是失败。这个项目只能搁置了

--- src/com/example/photogallery/FlickrFetchr.java
+++ src/com/example/photogallery/FlickrFetchr.java
@@ -6,9 +6,20 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.net.Uri;
 import android.util.Log;
 
 public class FlickrFetchr {
+	public static final String TAG = "FlickFetchr";
+	
+	private static final String ENDPOINT = "http://api.flickr.com/services/rest/";
+	private static final String API_KEY = "64009e2b43124ed4504bba9490a9052e";
+	private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+	private static final String PARAM_EXTRA = "extras";
+	
+	private static final String EXTRA_SMALL_URL = "url_s";
+	
+	
 	byte[] getUrlBytes(String urlSpec) throws IOException {
 		URL url = new URL(urlSpec);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -34,5 +45,20 @@ public class FlickrFetchr {
 	}
 	public String getUrl(String urlSpec) throws IOException {
 		return new String(getUrlBytes(urlSpec));
+	}
+	
+	public void fetchItems() {
+		try {
+			String url = Uri.parse(ENDPOINT).buildUpon()
+					.appendQueryParameter("method", METHOD_GET_RECENT)
+					.appendQueryParameter("api_key", API_KEY)
+					.appendQueryParameter(PARAM_EXTRA, EXTRA_SMALL_URL)
+					.build().toString();
+			Log.i(TAG, "The url: " + url);
+			String xmlString = getUrl(url);
+			Log.i(TAG, "Received xml: " + xmlString);
+		} catch (IOException ioe) {
+			Log.e(TAG, "Failed to fetch items", ioe);
+		}
 	}
 }
